007f99334987dd9a94363fc8617845c5ee55e2e4
Ehabben10 <202107666@post.au.dk>
2023-12-16 11:58:35 +0100

    making the side bar nice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { gql, useQuery } from '@apollo/react-hooks'
 import { PageHeader } from './layouts/PageHeader'
-import { Sidebar } from './layouts/SideBar'
 import { SideBarProvider } from './context/SideBarcontext'
 import { ShiftGridItem } from './components/ui/ShiftGridItem'
+import { SideBar } from './layouts/Sidebar'
 
 export const test = gql`
 query GetEvent{
@@ -23,26 +23,24 @@ function App() {
   const { data, loading, error } = useQuery(test);
   console.log(data);
   return (
-    <SideBarProvider>
-      <div className="max-h-screen flex flex-col">
-        <PageHeader />
-        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-          <Sidebar />
-          <div className="overflow-x-hidden px-8 pb-4">
-            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-              {data?.get.map((shift: any) => (
-                <ShiftGridItem
-                  Date={shift.start.dateTimeDateTimeOffset}
-                  Hours="Antale timer: 8"
-                  Time="Du skal arbejde fra 10:00 til 12:00"
-                  Description={shift.description}
-                />))
-              }
-            </div>
-          </div>
-        </div>
-      </div>
-    </SideBarProvider>
+
+    // <div className="max-h-screen flex flex-col">
+    //   <PageHeader />
+    //   <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+    //     <div className="overflow-x-hidden px-8 pb-4">
+    //       <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+    //         {data?.get.map((shift: any) => (
+    //           <ShiftGridItem
+    //             Date={shift.start.dateTimeDateTimeOffset}
+    //             Hours="Antale timer: 8"
+    //             Time="Du skal arbejde fra 10:00 til 12:00"
+    //             Description={shift.description}
+    //           />))
+    //         }
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx"
 import "./index.css"
 import { ApolloProvider } from '@apollo/react-hooks'
 import client from './apollo/client.tsx'
+import { SideBar } from "./layouts/Sidebar.tsx"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <SideBar />
+
     </ApolloProvider >
   </React.StrictMode>,
 )
